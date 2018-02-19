ef6a8d77ebecb2bbc25188f776cef8b91534f9f4
Rakhim Davletkaliyev <freetonik@gmail.com>
2018-02-19 14:56:31 +0200

    remove null, add testing, add edits

--- modules/10-basics/45-testing/Test.php
+++ modules/10-basics/45-testing/Test.php
@@ -8,7 +8,8 @@ use function HexletBasics\Asserts\expectOutputString;
 
 $path = __DIR__ . '/index.php';
 
-$expected = 'NULL';
+$expected = "9780262531962";
+print_r("Expected value: {$expected}\n  Actual value: ");
 require $path;
 
 expectOutputString($expected, function () use ($path) {
