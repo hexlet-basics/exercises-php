7e9abfb5e5bc2e2e4904970e50888d4444e45993
Rakhim Davletkaliyev <freetonik@gmail.com>
2018-03-14 12:13:53 +0200

    fix links and add info about modulo op

--- drafts/50-loops/30-edge-cases/index.php
+++ drafts/50-loops/30-edge-cases/index.php
@@ -5,6 +5,12 @@ function isArgumentsForSubstrCorrect($str, $index, $length)
     if ($index < 0) {
         return false;
     }
+    if ($length < 0) {
+        return false;
+    }
+    if ($index > strlen($str) - 1) {
+        return false;
+    }
     if ($index + $length > strlen($str)) {
         return false;
     }
