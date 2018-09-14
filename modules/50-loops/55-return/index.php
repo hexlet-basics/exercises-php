2a48a71781a51c2f9783722d8f1bd5b630309285
Kirill Mokevnin <mokevnin@gmail.com>
2018-09-14 10:18:19 +0200

    fix solution for loops/return

--- modules/50-loops/55-return/index.php
+++ modules/50-loops/55-return/index.php
@@ -3,7 +3,7 @@
 function doesContain($str, $char)
 {
     $index = 0;
-    while ($index <= strlen($str) - 1) {
+    while ($index < strlen($str)) {
         if ($str[$index] === $char) {
             return true;
         }
