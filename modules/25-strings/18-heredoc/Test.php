834a8797c347077d0ff01af879c057d94dd94594
Kirill Mokevnin <mokevnin@gmail.com>
2018-10-01 18:01:16 +0300

    add heredoc to php

---
+++ modules/25-strings/18-heredoc/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HexletBasics;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use function HexletBasics\Asserts\expectOutputString;
+
+$path = __DIR__ . '/index.php';
+
+$expected = <<<HERE
+Lannister, Targaryen, Baratheon, Stark, Tyrell... they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins, crushing those on the ground.
+HERE;
+
+require $path;
+
+expectOutputString($expected, function () use ($path) {
+    require $path;
+});
---
+++ modules/25-strings/18-heredoc/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// BEGIN
+$str = <<<HERE
+Lannister, Targaryen, Baratheon, Stark, Tyrell... they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins, crushing those on the ground.
+HERE;
+// END
+
+print_r($str);
