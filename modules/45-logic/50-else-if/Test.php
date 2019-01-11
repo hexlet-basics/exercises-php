66ef38c6a4ef2663c71138d6754211aa5fe0f2b6
Oliverity <46054994+Oliverity@users.noreply.github.com>
2019-01-11 16:55:59 +0300

    Lords of Riverrun were named 'Tully'. (#51)
    * Update Test.php
    
    * Update description.ru.yml

--- modules/45-logic/50-else-if/Test.php
+++ modules/45-logic/50-else-if/Test.php
@@ -9,7 +9,7 @@ use Assert\Assert;
 $path = __DIR__ . '/index.php';
 require $path;
 
-Assert::that(whoIsThisHouseToStarks('Tally'))->eq('friend');
+Assert::that(whoIsThisHouseToStarks('Tully'))->eq('friend');
 Assert::that(whoIsThisHouseToStarks('Karstark'))->eq('friend');
 Assert::that(whoIsThisHouseToStarks('Lannister'))->eq('enemy');
 Assert::that(whoIsThisHouseToStarks('Martell'))->eq('neutral');
