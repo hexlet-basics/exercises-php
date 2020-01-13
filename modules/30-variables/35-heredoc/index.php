391d92d5d88695d2598354e0965136de9d42eac9
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-01-13 18:58:24 +0200

    fix(35-heredoc): HERE to EOT & remove spaces

--- modules/30-variables/35-heredoc/index.php
+++ modules/30-variables/35-heredoc/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // BEGIN
-$str = <<<HERE
+$str = <<<EOT
 Lannister, Targaryen, Baratheon, Stark, Tyrell... they're all just spokes on a wheel.
 This one's on top, then that one's on top, and on and on it spins, crushing those on the ground.
-HERE;
+EOT;
 // END
 
 print_r($str);
