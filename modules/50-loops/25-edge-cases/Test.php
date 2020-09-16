c839c8d5127a740216c0400746822e0b8018dbc3
Roman Ashikov <ashikov@users.noreply.github.com>
2020-09-17 01:18:00 +0700

    (50/25) refactored tests, add return type check (#180)
    * (50/25) refactored tests, add return type check
    
    * (50/25) fix asserts in tests

--- modules/50-loops/25-edge-cases/Test.php
+++ modules/50-loops/25-edge-cases/Test.php
@@ -11,12 +11,33 @@ class Test extends TestCase
         require 'index.php';
 
         $str = 'Sansa Stark';
-        assert(!isArgumentsForSubstrCorrect($str, -1, 3));
-        assert(!isArgumentsForSubstrCorrect($str, 4, 100));
-        assert(!isArgumentsForSubstrCorrect($str, 10, 10));
-        assert(!isArgumentsForSubstrCorrect($str, 11, 1));
-        assert(!isArgumentsForSubstrCorrect($str, 11, 0));
-        assert(isArgumentsForSubstrCorrect($str, 3, 3));
-        assert(isArgumentsForSubstrCorrect($str, 10, 1));
+
+        $result1 = isArgumentsForSubstrCorrect($str, -1, 3);
+        $this->assertIsBool($result1);
+        $this->assertFalse($result1);
+
+        $result2 = isArgumentsForSubstrCorrect($str, 4, 100);
+        $this->assertIsBool($result2);
+        $this->assertFalse($result2);
+
+        $result3 = isArgumentsForSubstrCorrect($str, 10, 10);
+        $this->assertIsBool($result3);
+        $this->assertFalse($result3);
+
+        $result4 = isArgumentsForSubstrCorrect($str, 11, 1);
+        $this->assertIsBool($result4);
+        $this->assertFalse($result4);
+
+        $result5 = isArgumentsForSubstrCorrect($str, 11, 0);
+        $this->assertIsBool($result5);
+        $this->assertFalse($result5);
+
+        $result6 = isArgumentsForSubstrCorrect($str, 3, 3);
+        $this->assertIsBool($result6);
+        $this->assertTrue($result6);
+
+        $result7 = isArgumentsForSubstrCorrect($str, 10, 1);
+        $this->assertIsBool($result7);
+        $this->assertTrue($result7);
     }
 }
