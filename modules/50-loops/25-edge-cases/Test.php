60f3f11ed0a78717646272949e5d0b70ac4fe07c
mokevnin <mokevnin@gmail.com>
2020-09-21 22:19:41 -0400

    improve tests

--- modules/50-loops/25-edge-cases/Test.php
+++ modules/50-loops/25-edge-cases/Test.php
@@ -12,32 +12,12 @@ class Test extends TestCase
 
         $str = 'Sansa Stark';
 
-        $result1 = isArgumentsForSubstrCorrect($str, -1, 3);
-        $this->assertIsBool($result1);
-        $this->assertFalse($result1);
-
-        $result2 = isArgumentsForSubstrCorrect($str, 4, 100);
-        $this->assertIsBool($result2);
-        $this->assertFalse($result2);
-
-        $result3 = isArgumentsForSubstrCorrect($str, 10, 10);
-        $this->assertIsBool($result3);
-        $this->assertFalse($result3);
-
-        $result4 = isArgumentsForSubstrCorrect($str, 11, 1);
-        $this->assertIsBool($result4);
-        $this->assertFalse($result4);
-
-        $result5 = isArgumentsForSubstrCorrect($str, 11, 0);
-        $this->assertIsBool($result5);
-        $this->assertFalse($result5);
-
-        $result6 = isArgumentsForSubstrCorrect($str, 3, 3);
-        $this->assertIsBool($result6);
-        $this->assertTrue($result6);
-
-        $result7 = isArgumentsForSubstrCorrect($str, 10, 1);
-        $this->assertIsBool($result7);
-        $this->assertTrue($result7);
+        assert(isArgumentsForSubstrCorrect($str, -1, 3) === false);
+        assert(isArgumentsForSubstrCorrect($str, 4, 100) === false);
+        assert(isArgumentsForSubstrCorrect($str, 10, 10) === false);
+        assert(isArgumentsForSubstrCorrect($str, 11, 1) === false);
+        assert(isArgumentsForSubstrCorrect($str, 11, 0) === false);
+        assert(isArgumentsForSubstrCorrect($str, 3, 3) === true);
+        assert(isArgumentsForSubstrCorrect($str, 10, 1) === true);
     }
 }
