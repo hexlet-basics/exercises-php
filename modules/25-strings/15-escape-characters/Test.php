b32ffc50972cc460977d2ab7a50e7099e4343ae6
Roman Ashikov <rmn.shkv@gmail.com>
2020-09-23 20:26:16 +0700

    improve tests

--- modules/25-strings/15-escape-characters/Test.php
+++ modules/25-strings/15-escape-characters/Test.php
@@ -1,8 +1,10 @@
-<?php
+<?php // phpcs:ignore PSR1.Files.SideEffects
 
 namespace HexletBasics\Arithmetics\EscapeCharacters;
 
 use PHPUnit\Framework\TestCase;
+
+\HexletBasics\Functions\runScript();
 
 class Test extends TestCase
 {
