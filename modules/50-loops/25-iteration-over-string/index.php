d97062daa8dc27c68fd1cc6d65e1832816312090
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-11-16 14:06:24 +0300

    fix(50-loops/25-iteration-over-string): add line break in solution

--- modules/50-loops/25-iteration-over-string/index.php
+++ modules/50-loops/25-iteration-over-string/index.php
@@ -7,7 +7,7 @@ function printReversedNameBySymbol($name)
 {
     $i = strlen($name) - 1;
     while ($i >= 0) {
-        print_r($name[$i]);
+        print_r("$name[$i]\n");
         $i = $i - 1;
     }
 }
