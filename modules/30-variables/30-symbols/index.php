bf2a415e9bee4bbb83371f90f5ba285d31dd4f2e
Aleksandr Litvinov <ssssank@gmail.com>
2022-03-15 15:54:53 +0300

    Replace Game Of Thrones names

--- modules/30-variables/30-symbols/index.php
+++ modules/30-variables/30-symbols/index.php
@@ -1,9 +1,9 @@
 <?php
 
-$one = 'Naharis';
-$two = 'Mormont';
-$three = 'Sand';
+$one = 'Alexander';
+$two = 'Elena';
+$three = 'Ivan';
 
 // BEGIN
-print_r("{$one[2]}{$two[1]}{$three[3]}{$two[4]}{$two[2]}");
+print_r("{$one[8]}{$two[2]}{$two[1]}{$three[2]}{$one[3]}");
 // END
