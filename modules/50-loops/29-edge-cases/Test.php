eb319a5c0cd4f404de50ab3b841eaf571f2a232d
Aleksandr Litvinov <ssssank@gmail.com>
2022-03-15 17:42:53 +0300

    Replace Game Of Thrones

--- modules/50-loops/29-edge-cases/Test.php
+++ modules/50-loops/29-edge-cases/Test.php
@@ -10,13 +10,13 @@ class Test extends TestCase
     {
         require 'index.php';
 
-        $str = 'Sansa Stark';
+        $str = 'Proxima Centauri';
 
         assert(isArgumentsForSubstrCorrect($str, -1, 3) === false);
         assert(isArgumentsForSubstrCorrect($str, 4, 100) === false);
         assert(isArgumentsForSubstrCorrect($str, 10, 10) === false);
-        assert(isArgumentsForSubstrCorrect($str, 11, 1) === false);
-        assert(isArgumentsForSubstrCorrect($str, 11, 0) === false);
+        assert(isArgumentsForSubstrCorrect($str, 17, 1) === false);
+        assert(isArgumentsForSubstrCorrect($str, 17, 0) === false);
         assert(isArgumentsForSubstrCorrect($str, 3, 3) === true);
         assert(isArgumentsForSubstrCorrect($str, 10, 1) === true);
     }
